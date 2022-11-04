0034f8cc4c826e25aee42e5eafd1f011149c318d
daaguirre <da.aguirreg@gmail.com>
2022-11-04 14:55:22 +0100

    activate venv from path instead of name

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -3,25 +3,24 @@
 # if virtual env does not exist is created
 # later venv bin path is added to the PATH 
 # and project requirements are installed if the venv is a brand new venv
-# \arg VENV_NAME: name of virtual environment 
+# \arg VENV_PATH: path to venv to avtivate 
 # Output variables:
 # project_venv
 function(activate_virtual_env)
     set(options "")
-    set(oneValueArgs VENV_NAME)
+    set(oneValueArgs VENV_PATH)
     set(multiValueArgs "")
     cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    set(project_venv "${VENV_DIR}/${ARGS_VENV_NAME}" CACHE INTERNAL "")
-    if(NOT EXISTS "${project_venv}")
+    if(NOT EXISTS "${VENV_PATH}")
         find_package (Python3 3.8 COMPONENTS Interpreter)
-        message("Creating python venv in ${project_venv}")
-        execute_process (COMMAND "${Python3_EXECUTABLE}" -m venv "${project_venv}")
+        message("Creating python venv in ${VENV_PATH}")
+        execute_process (COMMAND "${Python3_EXECUTABLE}" -m venv "${VENV_PATH}")
         set(install_requirements true)
     endif()
 
     # activate python venv
-    set(ENV{VIRTUAL_ENV} ${project_venv})
+    set(ENV{VIRTUAL_ENV} ${VENV_PATH})
     set (Python3_FIND_VIRTUALENV FIRST)
     # unset Python3_EXECUTABLE because it is also an input variable (see documentation, Artifacts Specification section)
     unset (Python3_EXECUTABLE)
@@ -29,5 +28,5 @@ function(activate_virtual_env)
     if(install_requirements)
         execute_process (COMMAND "${Python3_EXECUTABLE}" -m pip install -r "${PROJECT_SOURCE_DIR}/python/requirements.txt")
     endif()
-    set(ENV{PATH} "$ENV{PATH}:${project_venv}/bin")
+    set(ENV{PATH} "$ENV{PATH}:${VENV_PATH}/bin")
 endfunction()
